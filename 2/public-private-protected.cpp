dee8da72978c09808bb934f0008dc0e8a35985c9
iazadur <iamazadur@gmail.com>
2024-01-15 08:56:04 +0600

    feat: public private protected in cpp

---
+++ 2/public-private-protected.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Parent
+{
+protected:
+	int id_protected;
+
+private:
+	double radius;
+
+public:
+	void compute_area(double r)
+	{
+		radius = r;
+		double area = 3.14 * radius * radius;
+		cout << "Radius is: " << radius << endl;
+		cout << "Area is: " << area << endl;
+	}
+};
+
+// Subclass or derived class from the base class
+class Child : public Parent
+{
+public:
+	void setId(int id)
+	{
+		id_protected = id;
+	}
+
+	void displayId()
+	{
+		cout << "id_protected is: " << id_protected << endl;
+	}
+};
+
+// Main function
+int main()
+{
+	Child obj1;
+	obj1.compute_area(1.5);
+	obj1.setId(81);
+	obj1.displayId();
+	return 0;
+}
