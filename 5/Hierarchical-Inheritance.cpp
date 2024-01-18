0830b597b692e86d4f273c4c20c70891d282d1a4
iazadur <iamazadur@gmail.com>
2024-01-18 21:08:19 +0600

    feat: issue fix on the code

--- 5/Hierarchical-Inheritance.cpp
+++ 5/Hierarchical-Inheritance.cpp
@@ -1,21 +1,18 @@
-// C++ program to implement
-// Hierarchical Inheritance
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-
 // base class
-class Vehicle {
+class Animal {
 public:
-	Vehicle() { cout << "This is a Vehicle\n"; }
+	Animal() { cout << "This is an Animal\n"; }
 };
 
 // first sub class
-class Car : public Vehicle {
+class Dog : public Animal {
 };
 
 // second sub class
-class Bus : public Vehicle {
+class Cat : public Animal {
 };
 
 // main function
@@ -23,8 +20,7 @@ int main()
 {
 	// Creating object of sub class will
 	// invoke the constructor of base class.
-	Car obj1;
-	Bus obj2;
+	Dog dogObj;
+	Cat catObj;
 	return 0;
 }
-
